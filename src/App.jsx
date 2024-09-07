e692c14d6241d352fe917cb2976ca0419ee60c25
hasanihardik <hasanihardik@gmail.com>
2024-09-07 17:05:46 +0530

    app and index config

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Sidebar, Navbar } from "./components";
+import { Home, Profile, Onboarding } from "./pages";
+import MedicalRecords from "./pages/records/index";
+import ScreeningSchedule from "./pages/ScreeningSchedule";
+import SingleRecordDetails from "./pages/records/single-record-details";
+import { useStateContext } from "./context";
+
+const App = () => {
+  const { user, authenticated, ready, login, currentUser } = useStateContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login();
+    } else if (user && !currentUser) {
+      navigate("/onboarding");
+    }
+  }, [user, authenticated, ready, login, currentUser, navigate]);
+
+  return (
+    <div className="sm:-8 relative flex min-h-screen flex-row bg-[#13131a] p-4">
+      <div className="relative mr-10 hidden sm:flex">
+        <Sidebar />
+      </div>
+
+      <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/medical-records" element={<MedicalRecords />} />
+          <Route
+            path="/medical-records/:id"
+            element={<SingleRecordDetails />}
+          />
+          <Route path="/screening-schedules" element={<ScreeningSchedule />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+import { PrivyProvider } from "@privy-io/react-auth";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="cm0s2g0xa02ddc4ri3530zaud"
+    config={{
+      appearance: {
+        theme: "dark",
+      },
+      embeddedWallets: {
+        createOnLogin: "users-without-wallets",
+      },
+    }}
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </PrivyProvider>,
+);
